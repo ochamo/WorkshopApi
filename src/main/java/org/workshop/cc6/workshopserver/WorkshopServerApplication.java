864f20f20a3714b0c8cf66e623b5a0b0bc3901ec
ochamo <60203851+ochamo@users.noreply.github.com>
2022-09-05 21:55:08 -0600

    initial commit

---
+++ src/main/java/org/workshop/cc6/workshopserver/WorkshopServerApplication.java
@@ -0,0 +1,13 @@
+package org.workshop.cc6.workshopserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/workshop/cc6/workshopserver/WorkshopServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.workshop.cc6.workshopserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
