932034c4111d0491e9ec0e5008724be158b8bf82
ochamo <60203851+ochamo@users.noreply.github.com>
2022-10-01 23:43:39 -0600

    chore: move file

--- src/main/java/org/workshop/cc6/workshopserver/service/user/IUserService.java
+++ src/main/java/org/workshop/cc6/workshopserver/service/user/IUserService.java
@@ -1,9 +1,9 @@
 package org.workshop.cc6.workshopserver.service.user;
 
 import org.workshop.cc6.workshopserver.dto.user.LoginRequest;
+import org.workshop.cc6.workshopserver.dto.user.LoginResponse;
 
-public class IUserService {
-
+public interface IUserService {
     LoginResponse login(LoginRequest user);
 
 }
