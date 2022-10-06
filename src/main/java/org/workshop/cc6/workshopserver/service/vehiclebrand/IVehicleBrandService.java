7da2b95410a0c1fe7ab6a8c417492c0dafe9fe65
ochamo <60203851+ochamo@users.noreply.github.com>
2022-10-06 10:27:33 -0600

    feat: Add more requests

--- src/main/java/org/workshop/cc6/workshopserver/service/vehiclebrand/IVehicleBrandService.java
+++ src/main/java/org/workshop/cc6/workshopserver/service/vehiclebrand/IVehicleBrandService.java
@@ -3,9 +3,13 @@ package org.workshop.cc6.workshopserver.service.vehiclebrand;
 import org.workshop.cc6.workshopserver.dto.BaseResponse;
 import org.workshop.cc6.workshopserver.dto.brand.request.CreateBrandRequest;
 import org.workshop.cc6.workshopserver.dto.brand.request.CreateVehicleLineRequest;
+import org.workshop.cc6.workshopserver.dto.brand.request.UpdateBrandRequest;
 
 public interface IVehicleBrandService {
     BaseResponse createVehicleBrand(CreateBrandRequest req);
+    BaseResponse updateVehicleBrand(UpdateBrandRequest req);
+
+    List<>
 
     BaseResponse createVehicleLine(CreateVehicleLineRequest req);
 
